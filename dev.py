599c62bc49a34b782448ae8ae889c491c7746059
engr-usman <s.ahmad@bboxx.co.uk>
2021-11-07 14:09:20 +0500

    updated dev.py file with correct print

--- dev.py
+++ dev.py
@@ -1,5 +1,5 @@
-print("This is the dev branch changes")
+print("This is the dev1 branch changes")
 print("This is the dev2 branch changes")
 print("This is the dev3 branch changes")
 print("This is the dev4 branch changes")
-print("This is the dev55 branch changes")
+print("This is the dev5 branch changes")
